fd56a9a39b1b5e0c081ed893839c505da99c6d92
Rafaela <rafaelatanakacardoso@gmail.com>
2026-03-24 23:55:30 -0300

    Departamento criado

---
+++ src/main/java/com/tc/lista/Model/Departamento.java
@@ -0,0 +1,42 @@
+package com.tc.lista.Model;
+
+import jakarta.persistence.*;
+
+@Entity(name = "Departamento")
+@Table(name = "tb_departamento")
+public class Departamento {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nome_departamento")
+    private String nome;
+
+    @Column(name = "localizacao_departamento")
+    private String localizacao;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getLocalizacao() {
+        return localizacao;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setLocalizacao(String localizacao) {
+        this.localizacao = localizacao;
+    }
+}
