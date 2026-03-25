c857a2373ea69f07da35fef97a566f992c8ed6d0
Rafaela <rafaelatanakacardoso@gmail.com>
2026-03-25 00:18:57 -0300

    LivroController criado

---
+++ src/main/java/com/tc/lista/Controller/LivroController.java
@@ -0,0 +1,36 @@
+package com.tc.lista.Controller;
+
+import com.tc.lista.Model.Livro;
+import com.tc.lista.Service.LivroService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/livros")
+public class LivroController {
+
+    @Autowired
+    private LivroService service;
+
+    @PostMapping
+    public Livro criar(@RequestBody Livro livro) {
+        return service.salvar(livro);
+    }
+
+    @GetMapping
+    public List<Livro> listar() {
+        return service.listar();
+    }
+
+    @GetMapping("/{id}")
+    public Livro buscar(@PathVariable Long id) {
+        return service.buscarPorId(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletar(@PathVariable Long id) {
+        service.deletar(id);
+    }
+}
