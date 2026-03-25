28f934295f2431808075803b9271907ae93e09c0
Rafaela <rafaelatanakacardoso@gmail.com>
2026-03-25 00:21:35 -0300

    ProdutoService criado

---
+++ src/main/java/com/tc/lista/Service/ProdutoService.java
@@ -0,0 +1,31 @@
+package com.tc.lista.Service;
+
+import com.tc.lista.Model.Produto;
+import com.tc.lista.Repository.ProdutoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProdutoService {
+
+    @Autowired
+    private ProdutoRepository repository;
+
+    public Produto salvar(Produto produto) {
+        return repository.save(produto);
+    }
+
+    public List<Produto> listar() {
+        return repository.findAll();
+    }
+
+    public Produto buscarPorId(Long id) {
+        return repository.findById(id).orElse(null);
+    }
+
+    public void deletar(Long id) {
+        repository.deleteById(id);
+    }
+}
