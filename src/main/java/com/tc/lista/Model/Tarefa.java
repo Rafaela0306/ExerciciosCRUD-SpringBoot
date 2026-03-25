d15c003a216412cdd983566db05b9a2acdc94136
Rafaela <rafaelatanakacardoso@gmail.com>
2026-03-25 00:10:32 -0300

    Tarefa criado

---
+++ src/main/java/com/tc/lista/Model/Tarefa.java
@@ -0,0 +1,54 @@
+package com.tc.lista.Model;
+
+import jakarta.persistence.*;
+import java.time.LocalDate;
+
+@Entity(name = "Tarefa")
+@Table(name = "tb_tarefa")
+public class Tarefa {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "descricao_tarefa")
+    private String descricao;
+
+    @Column(name = "data_vencimento")
+    private LocalDate dataVencimento;
+
+    @Column(name = "status_concluida")
+    private Boolean concluida;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public LocalDate getDataVencimento() {
+        return dataVencimento;
+    }
+
+    public Boolean getConcluida() {
+        return concluida;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public void setDataVencimento(LocalDate dataVencimento) {
+        this.dataVencimento = dataVencimento;
+    }
+
+    public void setConcluida(Boolean concluida) {
+        this.concluida = concluida;
+    }
+}
