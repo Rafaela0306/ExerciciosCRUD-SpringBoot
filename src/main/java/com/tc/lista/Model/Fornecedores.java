9cae8a2c2911b8e8af1c15ba05f08355afd54c81
Rafaela <rafaelatanakacardoso@gmail.com>
2026-03-24 23:46:19 -0300

    Fornecedores criado

---
+++ src/main/java/com/tc/lista/Model/Fornecedores.java
@@ -0,0 +1,52 @@
+package com.tc.lista.Model;
+
+import jakarta.persistence.*;
+
+@Entity(name = "Fornecedores")
+@Table(name = "tb_fornecedores")
+public class Fornecedores {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nome_fantasia")
+    private String nomeFantasia;
+
+    @Column(name = "cnpj_fornecedores")
+    private String cnpj;
+
+    private String contato;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNomeFantasia() {
+        return nomeFantasia;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public String getContato() {
+        return contato;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setNomeFantasia(String nomeFantasia) {
+        this.nomeFantasia = nomeFantasia;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public void setContato(String contato) {
+        this.contato = contato;
+    }
+}
