3b14e86275b45737f0111edb6195a4392e82cd11
Rafaela <rafaelatanakacardoso@gmail.com>
2026-03-25 00:22:42 -0300

    ProdutoController criado

---
+++ src/main/java/com/tc/lista/Controller/ProdutoController.java
@@ -0,0 +1,36 @@
+package com.tc.lista.Controller;
+
+import com.tc.lista.Model.Produto;
+import com.tc.lista.Service.ProdutoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/produtos")
+public class ProdutoController {
+
+    @Autowired
+    private ProdutoService service;
+
+    @PostMapping
+    public Produto criar(@RequestBody Produto produto) {
+        return service.salvar(produto);
+    }
+
+    @GetMapping
+    public List<Produto> listar() {
+        return service.listar();
+    }
+
+    @GetMapping("/{id}")
+    public Produto buscar(@PathVariable Long id) {
+        return service.buscarPorId(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletar(@PathVariable Long id) {
+        service.deletar(id);
+    }
+}
